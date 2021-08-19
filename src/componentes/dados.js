13d7a6bb7d7cebb537b93a370685f80864137666
Pedro da Conceicao <pedrocardo007@hotmail.com>
2021-08-19 17:51:36 -0300

    Terceiro test

--- src/componentes/dados.js
+++ src/componentes/dados.js
@@ -7,6 +7,7 @@ export default function Dados(props){
             <p>Youtube:{props.youtube} </p>
             <p>Curso:{props.curso} </p>
             <p>Nada de mais</p>
+            <p>Teste 2</p>
         </section>
     )
 }
